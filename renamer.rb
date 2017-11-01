61f126286586b547fa3c9cc7167736b97d1e0f64
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2017-11-01 12:10:14 +0600

    adds directory option, use File#rename instead of FileUtils

--- renamer.rb
+++ renamer.rb
@@ -1,6 +1,5 @@
 require 'yaml'
 require 'optparse'
-require 'fileutils'
 
 ###################### CLI option parser ###############
 options = {}
@@ -12,23 +11,41 @@ OptionParser.new do |opts|
     options[:verbose] = v
   end
 
+  opts.on("-d", "--directory dir", "directory to look for files") do |dir|
+    options[:directory] = dir
+  end
+
   opts.on("-e", "--extension ext",
     "rename file with extension ext, without dot") do |ext|
     options[:extension] = ext
   end
 end.parse!
 
+# extension is required
 if options[:extension].nil?
   raise Kernel.abort("Error: Please specify extension with -e or --extension")
 end
 
+# extension is required
+if options[:directory].nil?
+  raise Kernel.abort("Error: Specify directory with -d or --directory")
+end
+
 ##################### File rename ###############
 SURAH_DB = File.open("surah_names.yml") { |file| YAML.load(file) }
+
+directory = options[:directory] + (options[:directory][-1] == "/" ? '' : "/")
 dotted_ext = "." + options[:extension]
 
-Dir.glob('*' + dotted_ext ) do |filename|
+Dir.glob(directory + "*" + dotted_ext) do |filename|
   s_number = filename.match(/[0-9]{1,3}/)[0]
-  new_name = s_number + "." + SURAH_DB[s_number.to_i] + dotted_ext
-  puts "Renaming #{filename} to #{new_name}"
-  FileUtils.mv(filename, new_name)
+  new_name = directory + s_number + "." + SURAH_DB[s_number.to_i] + dotted_ext
+
+  begin
+    if File.rename(filename, new_name).zero? # means success
+      puts "#{filename} -> #{new_name}"
+    end
+  rescue SystemCallError
+    puts "renamer: Failed to rename file! #{filename}"
+  end
 end
